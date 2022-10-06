65d6f1190b39d0974c1555890380a475a69ae116
Prithijit07 <royceroy43@gmail.com>
2022-10-06 19:10:29 +0530

    Java program to check whether a stack is empty or not

---
+++ stack/StackEmptyMethodExample.java
@@ -0,0 +1,16 @@
+import java.util.Stack;  
+public class StackEmptyMethodExample  
+{  
+public static void main(String[] args)   
+{  
+Stack<Integer> stk= new Stack<>();  
+boolean result = stk.empty();  
+System.out.println("Is the stack empty? " + result);  
+stk.push(113);  
+stk.push(90);  
+stk.push(120);  
+System.out.println("Elements in Stack: " + stk);  
+result = stk.empty();  
+System.out.println("Is the stack empty? " + result);  
+}  
+}  
