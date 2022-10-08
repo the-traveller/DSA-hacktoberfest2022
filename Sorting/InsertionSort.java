934b26b2ff7ffcc468671a858b6b0c09c3f9c07b
iNitialM503 <initial.m503@gmail.com>
2022-10-08 23:52:11 +0700

    Add Primes Number Sorting

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,4 +1,4 @@
-/* About-: This is an in-place comparison-based sorting algorithm. Here, a sub-list is maintained which is always
+package Sorting;/* About-: This is an in-place comparison-based sorting algorithm. Here, a sub-list is maintained which is always
   sorted. For example, the lower part of an array is maintained to be sorted. An element which is to be 'insert'ed 
  in this sorted sub-list, has to find its appropriate place and then it has to be inserted there. Hence the name, 
  insertion sort */
--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,3 +1,5 @@
+package Sorting;
+
 import java.util.Arrays;
 
 public class SelectionSort {
